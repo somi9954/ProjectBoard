82301e7a2001b569f36321bbbba1c1c9bfa34974
somi9954 <jolh999@gmail.com>
2023-12-16 11:41:24 +0900

    게시판 기능, 댓글 기능 추가중

---
+++ src/main/java/org/koreait/controllers/boards/BoardDataSearch.java
@@ -0,0 +1,13 @@
+package org.koreait.controllers.boards;
+
+import lombok.Data;
+
+@Data
+public class BoardDataSearch {
+    private String bId;
+    private int page = 1;
+    private int limit = 20;
+    private String category;
+    private String sopt;
+    private String skey;
+}
